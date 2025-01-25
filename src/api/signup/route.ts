32d7b12fd727416e73d20c65d3a3b0e86db5909a
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2025-01-25 21:38:33 +0530

    Add bcryptjs for password hashing and implement user signup route

---
+++ src/api/signup/route.ts
@@ -0,0 +1,23 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import bcrypt from "bcryptjs"
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+
+export async function POST(request: Request) {
+    await dbConnect()
+
+    try {
+        const {username, email, password} = await request.json()
+    } catch (error) {
+        console.log("Error registering user", error)
+        return Response.json ({
+            success:false,
+            message: "registering user"
+        },
+    {
+        status: 500
+    })
+    }
+}
